da95b6ad128128f2ce196dba360f7f1bee36bbaf
Michael Chow <mcmcmc426@gmail.com>
2016-08-29 16:50:35 -0700

    introducing angular front-end

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var initPos = {lat: 37.869260, lng: -122.254811};
 function initMap() {
   // Create a map object and specify the DOM element for display.
   map = new google.maps.Map(document.getElementById('map'), {
-      center: new google.maps.LatLng(37.869260, -122.254811),
-      zoom: 8
-    });
+    center: new google.maps.LatLng(37.869260, -122.254811),
+    zoom: 16
+  });
   //create marker for the map
   marker = new google.maps.Marker({
     map: map,
@@ -18,10 +18,9 @@ function initMap() {
   panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), {
     position: initPos,
     pov: {heading: 165, pitch: 0},
-    motionTrackingControlOptions: {
-      motionTracking: false,
-      motionTrackingControl: false
-    }
+    linksControl: false,
+    motionTracking: false,
+    motionTrackingControl: false
   });
 
   sv = new google.maps.StreetViewService();
@@ -34,13 +33,15 @@ function initMap() {
         heading: 180,
         pitch: 0
       });
+      
       panorama.setVisible(true);
       var lat = data.location.latLng.lat();
       var lng = data.location.latLng.lng();
-      var mapLocation = {lat: lat, lng: lng};
-      map.setCenter(mapLocation);
+      placeLocation = {lat: lat, lng: lng};
+      console.log(placeLocation);
+      map.setCenter(placeLocation);
       map.setZoom(16);
-      marker.setPosition(mapLocation);
+      marker.setPosition(placeLocation);
       marker.setVisible(true);
     } else {
       console.error('Street View data not found for this location.');
@@ -48,7 +49,7 @@ function initMap() {
   }
 
   var input = document.getElementById("searchLocation");
-  panorama.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  // panorama.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
   var autoComplete = new google.maps.places.Autocomplete(input);
   
   autoComplete.addListener('place_changed', function() {
@@ -56,24 +57,16 @@ function initMap() {
     placeLat = place.geometry.location.lat();
     placeLng = place.geometry.location.lng();
     placeLocation = {lat: placeLat, lng: placeLng};
-
     if (!place.geometry) {
       window.alert("Autocomplete's returned place contains no geometry");
       return;
+    } else {
+      sv.getPanorama({location: placeLocation, radius: 50}, processSVData);
     }
-    // If the place has a geometry, then present it on a map.
-    sv.getPanorama({location: placeLocation, radius: 50}, processSVData);
   });
 
   marker.addListener('mouseup', function(event) {
-    placeLocation = event.latLng
     sv.getPanorama({location: event.latLng, radius: 50}, processSVData);
   });
 
-  // panorama.addListener('pano_changed', function() {
-  //   var newLocation = panorama.getPosition()
-  //   map.setCenter(newLocation);
-  //   marker.setPosition(newLocation);
-  //   placeLocation = newLocation
-  // });
 };
---
+++ search/search.js
@@ -0,0 +1,76 @@
+angular.module('search', [])
+  .controller('searchCtrl', function($scope) {
+    var map, marker, panorama, sv, processSVData, placeLocation;
+    var initPos = {lat: 37.869260, lng: -122.254811};
+    function initMap() {
+      // Create a map object and specify the DOM element for display.
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: new google.maps.LatLng(37.869260, -122.254811),
+        zoom: 16
+      });
+      //create marker for the map
+      marker = new google.maps.Marker({
+        map: map,
+        position: initPos,
+        anchorPoint: new google.maps.Point(0, -29),
+        animation: google.maps.Animation.DROP,
+        draggable: true
+      });
+      //create panorama
+      panorama = new google.maps.StreetViewPanorama(document.getElementById('pano'), {
+        position: initPos,
+        pov: {heading: 165, pitch: 0},
+        linksControl: false,
+        motionTracking: false,
+        motionTrackingControl: false
+      });
+
+      sv = new google.maps.StreetViewService();
+
+      processSVData = function (data, status) {
+        if (status === 'OK') {
+
+          panorama.setPano(data.location.pano);
+          panorama.setPov({
+            heading: 180,
+            pitch: 0
+          });
+          
+          panorama.setVisible(true);
+          var lat = data.location.latLng.lat();
+          var lng = data.location.latLng.lng();
+          placeLocation = {lat: lat, lng: lng};
+          console.log(placeLocation);
+          map.setCenter(placeLocation);
+          map.setZoom(16);
+          marker.setPosition(placeLocation);
+          marker.setVisible(true);
+        } else {
+          console.error('Street View data not found for this location.');
+        }
+      }
+
+      var input = document.getElementById("searchLocation");
+      var autoComplete = new google.maps.places.Autocomplete(input);
+      
+      autoComplete.addListener('place_changed', function() {
+        var place = autoComplete.getPlace();
+        placeLat = place.geometry.location.lat();
+        placeLng = place.geometry.location.lng();
+        placeLocation = {lat: placeLat, lng: placeLng};
+        if (!place.geometry) {
+          window.alert("Autocomplete's returned place contains no geometry");
+          return;
+        } else {
+          sv.getPanorama({location: placeLocation, radius: 50}, processSVData);
+        }
+      });
+
+      marker.addListener('mouseup', function(event) {
+        sv.getPanorama({location: event.latLng, radius: 50}, processSVData);
+      });
+
+    };
+    
+  })
+
